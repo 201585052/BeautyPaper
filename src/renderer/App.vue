ffbaef74d77ae53225cfa2a34cb57f2dd7aba336
201585052 <1216884492@qq.com>
2018-08-18 11:40:43 +0800

    waitsetpaper

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <Carousel />
-    <div>{{imgs}}</div>
+    <Carousel v-bind:imgs="imgs"/>
   </div>
 </template>
 
--- src/renderer/components/Carousel.vue
+++ src/renderer/components/Carousel.vue
@@ -6,8 +6,9 @@
     <div class="bar">
       <span v-for="(item, index) in bannerList" :class="{ 'active':index===mark }" :key="index"></span>
     </div>
-    <div class="cleft"><img src="static/images/left.png"></div>
-    <div class="cright"><img src="static/images/right.png"></div>
+    <div class="chose">选做桌面</div>
+    <div class="cleft" @click="goLeft"><img src="static/images/left.png"></div>
+    <div class="cright" @click="goRight"><img src="static/images/right.png"></div>
   </div>
 </template>
 
@@ -16,25 +17,32 @@
     data () {
       return {
         mark: 0,
-        bannerList: ['static/images/1.jpg', 'static/images/2.jpg', 'static/images/3.jpg']
+        bannerList: this.imgs,
+        len: this.imgs.length
+      }
+    },
+    props: {
+      imgs: {
+        type: Array,
+        required: true
       }
     },
     methods: {
-      autoPlay () {
+      goRight () {
         this.mark++
-        if (this.mark === 3) {
+        if (this.mark === this.len) {
           this.mark = 0
         }
       },
-      play () {
-        setInterval(this.autoPlay, 2500)
+      goLeft () {
+        this.mark--
+        if (this.mark === 0) {
+          this.mark = this.len
+        }
       },
       change (i) {
         this.mark = i
       }
-    },
-    created () {
-      this.play()
     }
   }
 </script>
@@ -53,28 +61,58 @@
       background-size: cover;
     }
     .slideshow img{
-      width: 100%;
+      width: 100vw;
       height: 100vh;
     }
     li {
       position: absolute;
     }
+    .chose{
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100px;
+      height: 30px;
+      margin: auto;
+      border: 1px solid #fff;
+      padding: 10px;
+      font-weight: bold;
+      font-size: 16px;
+      color: #fff;
+      text-align: center;
+      line-height: 30px;
+      opacity: 0.3;
+      cursor: pointer;
+    }
+    .chose:hover{
+      color: cornflowerblue;
+      transition: all .5s ease;
+      opacity: 1;
+      background-color: #fff;
+      box-shadow: 1px 1px 10px #333333;
+    }
     .cleft{
       position: absolute;
-      top:45%;
+      top:50%;
+      transform: translateY(-50%);
+      width: 10vw;
       cursor: pointer;
     }
     .cleft img{
-      width: 10vw;
+      width: 100%;
     }
     .cright{
       position: absolute;
-      top:45%;
+      top:50%;
       right: 0;
+      width: 10vw;
+      transform: translateY(-50%);
       cursor: pointer;
     }
     .cright img{
-      width: 10vw;
+      width: 100%;
     }
     .bar {
       position: absolute;
